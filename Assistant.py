0304fd8daca05300a549b0fbf126201d1ce11178
Carter Brown <carterbrwn2@gmail.com>
2018-10-27 12:31:56 -0400

    Added Assistant.decode(), modified Assistant.notify()

--- Assistant.py
+++ Assistant.py
@@ -4,26 +4,43 @@ class Assistant:
 
     messages = {
         "tornado-warning":"""A tornado warning means that a tornado has been spotted
-                            or radar indicates the presence of a thunderstorm circulation
-                            that could result in a tornado. Please take immediate action:
+or radar indicates the presence of a thunderstorm circulation
+that could result in a tornado. Please take immediate action:
 
-                            If you are outside, immediately seek shelter in a sturdy building.
+If you are outside, immediately seek shelter in a sturdy building.
                             
-                            If you are inside, move to an interior room on the lowest floor.
+If you are inside, move to an interior room on the lowest floor.
                             
-                            Avoid windows.
+Avoid windows.
                             
-                            If no shelter is available, lie down in a low-lying area.
+If no shelter is available, lie down in a low-lying area.
                             
-                            Protect yourself from flying debris."""
+Protect yourself from flying debris."""
     }
 
     flags = {
         "tornado-warning": False
     }
 
+    msg_history = []
+
     def __init__(self):
         self.notify("Assistant started")
 
-    def notify(self, msg):
-        print(msg)
+    def notify(self, msg, msg_key=None):
+        if msg_key is None:
+            print(msg)
+        else:
+            print(self.messages[msg_key])
+
+    def decode(self, msg):
+        msg_arr = msg.split("&&")
+        command, arg, value = msg_arr[0:3]
+        if command == "notify":
+            self.notify(None, arg)
+        elif command == "setflag":
+            self.flags[arg] = value
+        else:
+            self.notify(command, None)
+
+
--- run_assistant.py
+++ run_assistant.py
@@ -1,13 +1,12 @@
 # Author: Carter Brown
+
 from Assistant import Assistant
 
 agent = Assistant()
 
 while 1:
     msg = input()
-
     if msg == "exit":
         print("Exiting...")
-        break
-    elif msg == "":
-        pass
+
+    agent.decode(msg)
