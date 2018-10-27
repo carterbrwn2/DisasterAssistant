3275145f5bf6021dc1a93f0baca85934d4e212a5
AlyKat16 <slahgnal.k.a@hotmail.com>
2018-10-27 15:45:45 -0400

    Added multi-threading to run program and gui at same time

--- GUI.py
+++ GUI.py
@@ -1,7 +1,42 @@
 import tkinter
-window = tkinter.Tk()
+import tkinter.scrolledtext
+import time
 
-window.title("ACTIVE ALERTS")
-window.wm_iconbitmap('assets/warning_im6_icon.ico')
-#<div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Warning">Warning</a> from <a href="https://www.flaticon.com/"     title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"     title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
-window.mainloop()
+
+
+class GUI:
+  displayedMessage = ""
+  scrolledtext = None
+
+  def __init__(self):
+    self = tkinter.Tk()
+    self.title("ACTIVE ALERTS")
+    self.wm_iconbitmap('assets/warning_im6_icon.ico')
+    # <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Warning">Warning</a> from <a href="https://www.flaticon.com/"     title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"     title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+    #self.geometry("%dx%d+0+0" % self.maxsize())
+
+    GUI.scrolledtext = tkinter.scrolledtext.ScrolledText(self)
+    GUI.scrolledtext.pack(fill="both", expand=True)
+    GUI.displayedMessage = "No current alerts"
+    GUI.scrolledtext.insert(tkinter.INSERT, GUI.displayedMessage)
+
+    self.mainloop()
+
+  def update_Display(message):
+    GUI.displayedMessage = time.ctime() + "\n" + message
+    GUI.scrolledtext.insert(tkinter.INSERT, GUI.displayedMessage)
+
+
+
+
+def create_GUI():
+    window = GUI()
+
+
+
+
+
+
+
+
+
--- run_assistant.py
+++ run_assistant.py
@@ -1,13 +1,30 @@
 # Author: Carter Brown, Alyssa Langhals
 
 from Assistant import Assistant
+from threading import Thread
+import GUI
 
-agent = Assistant()
+def main():
+  agent = Assistant()
 
-while 1:
-    msg = input()
-    if msg == "exit":
-        print("Exiting...")
-        break
+  while 1:
+      msg = input()
+      if msg == "exit":
 
-    agent.decode(msg)
+         print("Exiting...")
+         break
+      else:
+          GUI.GUI.update_Display(msg + "\n")
+      #agent.decode(msg)
+
+
+threadGUI = Thread(target = GUI.create_GUI)
+threadMain = Thread(target = main)
+threadMain.start()
+threadGUI.start()
+
+threadMain.join()
+print("ClosingMAIN")
+threadGUI.join()
+print("ClosingGUI")
+
