8ad0611e48e2aaa523178a94a4b5aa932841537c
Carter Brown <carterbrwn2@gmail.com>
2018-10-27 19:48:44 -0400

    Added documentation

--- GUI.py
+++ GUI.py
@@ -1,3 +1,5 @@
+# Author: Carter Brown, Alyssa Langhals
+
 import tkinter
 import tkinter.scrolledtext
 
@@ -6,6 +8,12 @@ class GUI:
     displayed_message = ""
 
     def __init__(self):
+        """
+        Constructor for GUI
+
+        Initializes tkinter window
+
+        """
         self.root = tkinter.Tk()
         self.root.attributes("-fullscreen", True)
         self.root.title("ACTIVE ALERTS")
@@ -14,11 +22,27 @@ class GUI:
         self.root.scrolledtext.pack(fill="both", expand=True)
 
     def update_display(self, message):
+        """
+        Updates the display with a received message
+
+        :param message: The message to display
+        :return: None
+        """
         self.displayed_message = message + "\n"
         self.root.scrolledtext.insert("0.0", self.displayed_message)
 
     def open_window(self):
+        """
+        Opens the tkinter window
+
+        :return: None
+        """
         self.root.mainloop()
 
     def close_window(self):
+        """
+        Closes the tkinter window
+
+        :return: None
+        """
         self.root.quit()
